ffd7b84467dd7052aa29b681995b6b06a6d456d6
Artem Subbotin <subbotin.artem@gmail.com>
2023-03-02 20:28:41 +0200

    deploy scripts was added

---
+++ scripts/deploy-implementations.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+//const HDWalletProvider = require('truffle-hdwallet-provider');
+
+function get_data(_message) {
+    return new Promise(function(resolve, reject) {
+        fs.readFile('./scripts/arguments.json', (err, data) => {
+            if (err) {
+				
+                if (err.code == 'ENOENT' && err.syscall == 'open' && err.errno == -4058) {
+                    fs.writeFile('./scripts/arguments.json', "", (err2) => {
+                        if (err2) throw err2;
+                        resolve();
+                    });
+                    data = ""
+                } else {
+                    throw err;
+                }
+            }
+    
+            resolve(data);
+        });
+    });
+}
+
+function write_data(_message) {
+    return new Promise(function(resolve, reject) {
+        fs.writeFile('./scripts/arguments.json', _message, (err) => {
+            if (err) throw err;
+            console.log('Data written to file');
+            resolve();
+        });
+    });
+}
+
+async function main() {
+	var data = await get_data();
+
+    var data_object_root = JSON.parse(data);
+	var data_object = {};
+	if (typeof data_object_root[hre.network.name] === 'undefined') {
+        data_object.time_created = Date.now()
+    } else {
+        data_object = data_object_root[hre.network.name];
+    }
+	//----------------
+
+	const [deployer] = await ethers.getSigners();
+	
+	const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+    const RELEASE_MANAGER = process.env.RELEASE_MANAGER;
+    
+	console.log(
+		"Deploying contracts with the account:",
+		deployer.address
+	);
+
+	// var options = {
+	// 	//gasPrice: ethers.utils.parseUnits('50', 'gwei'), 
+	// 	gasLimit: 10e6
+	// };
+
+    const deployerBalanceBefore = await deployer.getBalance();
+    console.log("Account balance:", (deployerBalanceBefore).toString());
+
+	const SubscriptionsManagerUpgradeableF = await ethers.getContractFactory("SubscriptionsManagerUpgradeable");
+
+	let implementationSubscriptionsManagerUpgradeable = await SubscriptionsManagerUpgradeableF.connect(deployer).deploy();
+    
+	console.log("Implementations:");
+	console.log("  SubscriptionsManagerUpgradeable deployed at:       ", implementationSubscriptionsManagerUpgradeable.address);
+
+	data_object.implementationSubscriptionsManagerUpgradeable 	= implementationSubscriptionsManagerUpgradeable.address;
+    data_object.releaseManager                  = RELEASE_MANAGER;
+
+	const deployerBalanceAfter = await deployer.getBalance();
+	console.log("Spent:", ethers.utils.formatEther(deployerBalanceBefore.sub(deployerBalanceAfter)));
+	console.log("gasPrice:", ethers.utils.formatUnits((await network.provider.send("eth_gasPrice")), "gwei")," gwei");
+
+	//---
+	const ts_updated = Date.now();
+    data_object.time_updated = ts_updated;
+    data_object_root[`${hre.network.name}`] = data_object;
+    data_object_root.time_updated = ts_updated;
+    let data_to_write = JSON.stringify(data_object_root, null, 2);
+	console.log(data_to_write);
+    await write_data(data_to_write);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+	console.error(error);
+	process.exit(1);
+  });
---
+++ scripts/deploy.js
@@ -0,0 +1,87 @@
+const fs = require('fs');
+//const HDWalletProvider = require('truffle-hdwallet-provider');
+
+function get_data(_message) {
+    return new Promise(function(resolve, reject) {
+        fs.readFile('./scripts/arguments.json', (err, data) => {
+            if (err) {
+                if (err.code == 'ENOENT' && err.syscall == 'open' && err.errno == -4058) {
+					let obj = {};
+					data = JSON.stringify(obj, null, "");
+                    fs.writeFile('./scripts/arguments.json', data, (err) => {
+                        if (err) throw err;
+                        resolve(data);
+                    });
+                } else {
+                    throw err;
+                }
+            } else {
+            	resolve(data);
+			}
+        });
+    });
+}
+
+async function main() {
+	var data = await get_data();
+    var data_object_root = JSON.parse(data);
+	if (typeof data_object_root[hre.network.name] === 'undefined') {
+		throw("Arguments file: missed data");
+    } else if (typeof data_object_root[hre.network.name] === 'undefined') {
+		throw("Arguments file: missed network data");
+    }
+	data_object = data_object_root[hre.network.name];
+	if (
+		typeof data_object.implementationSubscriptionsManagerUpgradeable === 'undefined' ||
+		typeof data_object.releaseManager === 'undefined' ||
+		!data_object.implementationSubscriptionsManagerUpgradeable ||
+		!data_object.releaseManager
+	) {
+		throw("Arguments file: wrong addresses");
+	}
+
+	const [deployer] = await ethers.getSigners();
+	
+	const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+	console.log(
+		"Deploying contracts with the account:",
+		deployer.address
+	);
+
+	var options = {
+		//gasPrice: ethers.utils.parseUnits('50', 'gwei'), 
+		//gasLimit: 5e6
+	};
+	let _params = [
+		data_object.implementationSubscriptionsManagerUpgradeable,
+		ZERO_ADDRESS, //costmanager
+		data_object.releaseManager
+	]
+	let params = [
+		..._params,
+		options
+	]
+
+    const deployerBalanceBefore = await deployer.getBalance();
+    console.log("Account balance:", (deployerBalanceBefore).toString());
+
+	const SubscriptionsManagerFactoryF = await ethers.getContractFactory("SubscriptionsManagerFactory");
+
+	this.factory = await SubscriptionsManagerFactoryF.connect(deployer).deploy(...params);
+
+	console.log("Factory deployed at:", this.factory.address);
+	console.log("with params:", [..._params]);
+
+	console.log("registered with release manager:", data_object.releaseManager);
+	
+	const deployerBalanceAfter = await deployer.getBalance();
+	console.log("Spent:", ethers.utils.formatEther(deployerBalanceBefore.sub(deployerBalanceAfter)));
+	console.log("gasPrice:", ethers.utils.formatUnits((await network.provider.send("eth_gasPrice")), "gwei")," gwei");
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+	console.error(error);
+	process.exit(1);
+  });
